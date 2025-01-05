f9e1886c80fe4dd4c13f0894258e14df52cb27be
Young Seo <youngseokim7@gmail.com>
2025-01-05 13:43:09 -0800

    commit all example files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./dist/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/template.html'
+        })
+    ],
+    devtool: 'inline-source-map',
+    mode: 'development',
+    module: {
+        rules:[
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
